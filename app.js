70305e73aa80cf066485681205d56fe0231b7eb3
vijesh1v <90212830+vijesh1v@users.noreply.github.com>
2024-11-18 22:40:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const port = process.env.port || 3000;
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-
-
-app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
